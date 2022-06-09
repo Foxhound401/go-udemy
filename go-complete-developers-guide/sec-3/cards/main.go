a7517199b13db4ec0024ca49f616ca7ae8e8d6f8
Phuc Phung <phungthienphuc18@gmail.com>
2022-06-10 00:52:48 +0700

    go complaining about declare but no use

---
+++ go-complete-developers-guide/sec-3/cards/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var card string = "Ace of Spades"
+	var test1 string = "string 1"
+	var test2 int = 1
+
+	string1 := "test string 1"
+	string2 := "test string 2"
+
+	count := 10
+
+	count = 20
+
+	fmt.Println(card)
+}
