359949546de9ff2d249089887264e059da553103
Phuc Phung <phungthienphuc18@gmail.com>
2022-06-10 01:12:00 +0700

    practice printing stuff

--- go-complete-developers-guide/sec-3/cards/main.go
+++ go-complete-developers-guide/sec-3/cards/main.go
@@ -4,15 +4,31 @@ import "fmt"
 
 func main() {
 	var card string = "Ace of Spades"
-	var test1 string = "string 1"
-	var test2 int = 1
 
-	string1 := "test string 1"
-	string2 := "test string 2"
+	// var cars int = 100
+	// var space_in_a_car float32 = 4.0
+	//
+	// var drivers = 30
+	//
+	// passengers := 90
+	//
+	// cars_not_driven := cars - drivers
+	//
+	// var cars_driven int = drivers
+	//
+	// carpool_capacity := cars_driven * int(space_in_a_car)
+	//
+	// var average_passengers_per_car int = passengers / cars_driven
 
-	count := 10
-
-	count = 20
+	// multiple := "multiple"
+	//
+	// fmt.Printf("There are %d cars available\n", cars)
+	// fmt.Printf("There are onlly %d drivers available\n", drivers)
+	// fmt.Printf("There will be %d empty cars today\n", cars_not_driven)
+	// fmt.Printf("We can transport %d people today\n", carpool_capacity)
+	// fmt.Printf("We have %d to carpool today.\n", average_passengers_per_car)
+	//
+	// fmt.Printf("Can I do %s variable in the middle, we have %d passengers and %d cars\n", multiple, passengers, cars)
 
 	fmt.Println(card)
 }
